0ba826f4481f57bd5dcecbd122b69f6fe5255286
byjos89 <byjoselsig@gmail.com>
2022-12-14 13:44:52 -0400

    feature(common):Login css to Material UI [pcg2-0001] by byjos89

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,41 +1,42 @@
-import React from 'react';
-import './login.css';
+import React from 'react'
+//import './login.css';
+
 import { Button } from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
-
+import useStyles from './multi.style';
 const Login = () => {
     
-
-    return <>
-        <div className="main">
-            <div className="logo">
-                <p className="d"></p>
+    const classes =useStyles();
+        return <>
+            <div className="main">
+                <div className="logo">
+                    <p className="d"></p>
+                </div>
+                <h3 className="logoL">Plagio Control</h3>
+                <h2 className="tit">Login Plagio Control</h2>
+                <p className="comment">Enter you email and password below</p>
+                <form >
+                    <div className="con">
+                        <label htmlFor="">EMAIL</label>
+                        <input className="in" type="text" placeholder="Email address" name="email" />
+                    </div>
+                    <div className="con">
+                        <label htmlFor="">PASSWORD</label>
+                        <a href="" className="forgot">Forgot password?</a>
+                        <input className="in" placeholder="Password" name="password" />
+                        <button className="eyeB">
+                            <img className="eye" src="https://cdn-icons-png.flaticon.com/512/7042/7042918.png" />
+                        </button>
+                    </div>
+                    <button className="button">Log In</button>
+                    <div>
+                        <a>No tienes una cuanta?</a>
+                        &nbsp;&nbsp;
+                        <a href="#" className="singup">Sing up</a>
+                    </div>
+                </form>
             </div>
-            <h3 className="logoL">Plagio Control</h3>
-            <h2 className="tit">Login Plagio Control</h2>
-            <p className="comment">Enter you email and password below</p>
-            <form >
-                <div className="con">
-                    <label htmlFor="">EMAIL</label>
-                    <input className="in" type="text" placeholder="Email address" name="email" />
-                </div>
-                <div className="con">
-                    <label htmlFor="">PASSWORD</label>
-                    <a href="" className="forgot">Forgot password?</a>
-                    <input className="in" placeholder="Password" name="password" />
-                    <button className="eyeB">
-                        <img className="eye" src="https://cdn-icons-png.flaticon.com/512/7042/7042918.png" />
-                    </button>
-                </div>
-                <button className="button">Log In</button>
-                <div>
-                    <a>No tienes una cuanta?</a>
-                    &nbsp;&nbsp;
-                    <a href="#" className="singup">Sing up</a>
-                </div>
-            </form>
-        </div>
-    </>
+        </>
 }
 export default Login;
